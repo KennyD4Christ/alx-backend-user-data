f0744b557dde8565a3138086a0fd6a8d7caf1518
KennyD4Christ <kennyd4christ@gmail.com>
2024-09-03 11:38:23 +0100

    basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,12 +3,22 @@
 Module for Basic Authentication management.
 """
 from api.v1.auth.auth import Auth
-import base64
 
 class BasicAuth(Auth):
     """BasicAuth class for basic authentication."""
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(
+        self, authorization_header: str
+    ) -> str:
+        """
+        Extracts the Base64 part of the Authorization header for Basic Authentication.
+        
+        Args:
+            authorization_header (str): The Authorization header string.
+        
+        Returns:
+            str: The Base64 encoded part of the Authorization header or None.
+        """
         if authorization_header is None:
             return None
         if not isinstance(authorization_header, str):
@@ -16,24 +26,3 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[len("Basic "):]
-
-    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
-        if base64_authorization_header is None:
-            return None
-        if not isinstance(base64_authorization_header, str):
-            return None
-        try:
-            decoded_bytes = base64.b64decode(base64_authorization_header)
-            return decoded_bytes.decode('utf-8')
-        except (base64.binascii.Error, UnicodeDecodeError):
-            return None
-
-    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
-        if decoded_base64_authorization_header is None:
-            return None, None
-        if not isinstance(decoded_base64_authorization_header, str):
-            return None, None
-        if ':' not in decoded_base64_authorization_header:
-            return None, None
-        email, password = decoded_base64_authorization_header.split(':', 1)
-        return email, password
